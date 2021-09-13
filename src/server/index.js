fa35ce438ee651777ecc635f2875b3b4ea2aea04
Jon Tran <trungt8998@gmail.com>
2021-09-13 10:06:27 -0500

    Setting up serive worker

--- src/server/index.js
+++ src/server/index.js
@@ -22,7 +22,7 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
+app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
