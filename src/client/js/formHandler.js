dc2a4d4f6bb8d4c67a9f57f4cf91594fa95f898c
Jon Tran <trungt8998@gmail.com>
2021-09-13 22:37:23 -0500

    update formHandler

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -2,17 +2,22 @@ function handleSubmit(event) {
     event.preventDefault()
 
     // check what text was put into the form field
-    let formText = document.getElementById('name').value
+    let url = document.getElementById('name').value
     // console.log(typeOf formText);
-    if (formText == '' || Client.checkUrl(formText) ) {
+    if (url == '' || Client.checkUrl(url) ) {
         return alert('Please enter a valid url')
     } 
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8081/meaningCloud', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: { 'Content-type': 'application/json' },
+        body: JSON.stringify({url: url})
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res
     })
 }
 
