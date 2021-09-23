768c8422662214d26cc8e56eadc0211a8f8c9cf3
Jon Tran <trungt8998@gmail.com>
2021-09-22 21:13:31 -0500

    Enable Service worker

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,7 +4,7 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
 const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
 const TerserPlugin = require("terser-webpack-plugin")
-// const WorkboxPlugin = require('workbox-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -43,6 +43,6 @@ module.exports = {
             filename: "[name].css"
         }),
 
-        // new WorkboxPlugin.GenerateSW()
+        new WorkboxPlugin.GenerateSW()
     ]
 }
